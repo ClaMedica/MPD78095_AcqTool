bcd9780a483f7b4cb2726a3bad99deb42897e46a
cico giorgio <none@none>
2018-02-21 11:56:22 +0100

    media su flw e vol compressi

--- CPP/printermanager.cpp
+++ CPP/printermanager.cpp
@@ -185,8 +185,25 @@ void printermanager::pri_rep_review()
                 int n = m_dfm->GetSamplesNumber(c);
                 double  * vtmp = new double[n];
                 m_dfm->GetChVal(c, 0, n, vtmp, 0);
-                for(int i = 0; i < m_realDots; i++) // decimazione campioni
-                    buffer_flw[(int)(i*xscale)] = vtmp[i];
+                int ilast = -1; // indice su cui accumula la media, riferito a NUMOF_X_PRINT_DOTS
+                int icnt  = 0;
+                double isum = 0.0;
+                for(int i = 0; i < m_realDots; i++) {   // decimazione campioni
+                    double v = vtmp[i];
+                    v = (v < 0.0) ? -v : v;
+                    int ivirt = (int)(i*xscale);   // virtual index
+                    if(ilast < 0)
+                        ilast = ivirt;
+                    if(ivirt != ilast) {    // nuova posizione: scaricare la media in [ilast]
+                        buffer_flw[ilast] = isum / icnt;    // scarica valore precedente
+                        isum = 0.0;     // nuova posizione: media a 0.0
+                        icnt = 0;
+                        ilast = ivirt;
+                    }
+                    // ivirt == ilast
+                    isum += v;          // aggiorna la media
+                    icnt++;
+                }
                 qDebug("fine get flw");
             }
             if (chName.startsWith("VV") || chName ==  "VLMv" ) {
@@ -194,8 +211,25 @@ void printermanager::pri_rep_review()
                 int n = m_dfm->GetSamplesNumber(c);
                 double  * vtmp = new double[n];
                 m_dfm->GetChVal(c, 0, n, vtmp, 0);
-                for(int i = 0; i < m_realDots; i++) // decimazione campioni
-                    buffer_vol[(int)(i*xscale)] = vtmp[i];
+                int ilast = -1; // indice su cui accumula la media, riferito a NUMOF_X_PRINT_DOTS
+                int icnt  = 0;
+                double isum = 0.0;
+                for(int i = 0; i < m_realDots; i++) {   // decimazione campioni
+                    double v = vtmp[i];
+                    v = (v < 0.0) ? -v : v;
+                    int ivirt = (int)(i*xscale);   // virtual index
+                    if(ilast < 0)
+                        ilast = ivirt;
+                    if(ivirt != ilast) {    // nuova posizione: scaricare la media in [ilast]
+                        buffer_vol[ilast] = isum / icnt;    // scarica valore precedente
+                        isum = 0.0;     // nuova posizione: media a 0.0
+                        icnt = 0;
+                        ilast = ivirt;
+                    }
+                    // ivirt == ilast
+                    isum += v;          // aggiorna la media
+                    icnt++;
+                }
                 qDebug("fine get vol");
             }
         }
