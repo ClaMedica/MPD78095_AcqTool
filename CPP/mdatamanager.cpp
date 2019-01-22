5b8d06ded22704aeb95b48ec9f976c66398f067d
falcioni <none@none>
2019-01-22 09:55:44 +0100

    tolti commenti non necessari

--- CPP/mdatamanager.cpp
+++ CPP/mdatamanager.cpp
@@ -1430,7 +1430,7 @@ void MDataManager::startPrint()
 
 void MDataManager::udpMdmBtDecode(enum WHO __from, QByteArray __msg)
 {
-    qDebug() << __from << __msg;
+  //  qDebug() << __from << __msg;
 
     char cmd = __msg.at(0);
     switch(__from) {
