79c76f99adfb96383c4372e3f2fbc0cc57c8f632
cico giorgio <none@none>
2019-07-04 15:41:43 +0200

    debug su otherString

--- CPP/mdatamanager.cpp
+++ CPP/mdatamanager.cpp
@@ -172,15 +172,18 @@ void MDataManager::loadFile(QString __fileName)
         QStringList stringSplit = otherString.split(";");
         if (otherString == "")
         {
+            qDebug() << "otherstring vuota" << m_datiCalib;
             //c'è qualche problema e manca la calibrazione
             //la inseriamo
             otherString = m_datiCalib;
             m_mng->SetOther(otherString);
             m_mng->CommitParameters();
+            qDebug() << "otherstring vuota" << m_mng->GetOther();
         }
         else if (stringSplit.length() >= 2)//qui è tutto ok
         {
             QString first = stringSplit.at(0);
+            qDebug() << "first:" << first;
             if (first.contains("none") || first.length() >= 5)
             {
                 m_datiCalib = first;
@@ -963,8 +966,11 @@ bool MDataManager::checkForVolRes()
 
     //gestione campo Other del file .pic
     QString otherString = m_mng->GetOther();
-    QStringList stringSplit = otherString.split(";");
     qDebug() << "m_mng->GetOther() ==" << otherString;
+    if(otherString == "none")
+        otherString  += ";";
+    QStringList stringSplit = otherString.split(";");
+    qDebug() << "m_mng->GetOther() ==" << otherString << stringSplit;
     if (stringSplit.length() == 2)
     {
         otherString  += "0;" + QString::number(m_autoFlow) + ";";
@@ -1033,7 +1039,7 @@ qDebug() << "INIZIO";
     qDebug() << "File Aperto?" << m_mng->Open();
     qDebug() << "File Caricato?" << m_mng->GetParameters();
 
-    //mi salvo nel campo other il valore del volume residuo nel caso l'utente lo avesse cambiato
+    //mi salvo nel campo other il valore del volume residuo nel caso l'utente lo avesse nonecambiato
     QString other = m_mng->GetOther();
     QStringList otherList = other.split(";");
     m_datiCalib = otherList.at(0);//stringa per dati calibrazione da stampare
@@ -1058,7 +1064,6 @@ qDebug() << "INIZIO";
 
     for (int i = 0; i < m_numAna; i++) {
         int anaType = m_mng->GetAnalysis(i).toInt();
-
         if (anaType == FLW_AVD_STUDY) {
             //verifica se c'e' un definitore per questa analisi.
             VarMap mkOpAnIn;
